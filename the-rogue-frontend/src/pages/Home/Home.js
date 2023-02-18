1753ecea2493d07452362d5e1a8cb3af25332270
lmx97 <meixian971@gmail.com>
2023-02-18 18:14:12 +0800

    Amend Logo Size

--- the-rogue-frontend/src/pages/Home/Home.js
+++ the-rogue-frontend/src/pages/Home/Home.js
@@ -48,13 +48,4 @@ const Home = () => {
     );
 };
 
-const NumberText = ({num, text}) => {
-    return (
-        <div className="--mr">
-            <h3 className="--color-white">{num}</h3>
-            <p className="--color-white">{text}</p>
-        </div>
-    );
-};
-
 export default Home
