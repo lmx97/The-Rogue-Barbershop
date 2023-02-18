c9d79789f6eb998d75de2c7ce2420192ff22df9a
lmx97 <meixian971@gmail.com>
2023-02-18 18:49:58 +0800

    Create Story Page

--- the-rogue-frontend/src/App.js
+++ the-rogue-frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Services from "./pages/Services/Services";
+import Story from "./pages/Story/Story";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/story" element={<Story/>} />
         <Route path="/services" element={<Services/>} />
       </Routes>
     </BrowserRouter>
