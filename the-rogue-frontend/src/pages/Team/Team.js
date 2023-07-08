5cad30dea348be54335efad0deaa0df72118be84
lmx97 <meixian971@gmail.com>
2023-07-08 18:09:02 +0800

    edit margin

--- the-rogue-frontend/src/pages/Team/Team.js
+++ the-rogue-frontend/src/pages/Team/Team.js
@@ -9,62 +9,46 @@ const Team = () => {
   return (
     <div className="team" id="team" >
       <section className="team-header">
-        <h3>Meet Our Team</h3>
+        <h3><b>MEET OUR TEAM</b></h3>
         <p>The barbers you'd trust with your hair and your life. 
         You may know us before, <br/> or maybe you're a new face.
         Either way, you can trust us with yours. Welcome to the family.
         </p>
       </section>
-      <section className="team-links">
-        <div className="team-text">
-          <div className="team-icon">
-            <BsFillFileEarmarkPersonFill size={180} /> <br/>
-          </div>
-          <h4>Harry Chan </h4>
-          <h5>Master Barber</h5>
-          <br/>
-          <p>
-            Experience...
-          </p>
-        </div>
-        <br/> <br/>
+      <div className="team-links">
         <div className="team-text">
           <div className="team-icon">
             <BsFillFileEarmarkPersonFill size={180} />
+            <br/>
+            <h4>Harry</h4>
+            <h5>Master Barber</h5>
           </div>
-          <h4>Employee 1</h4>
-          <h5>Position</h5>
-          <br/>
-          <p>
-            Experience...
-          </p>
         </div>
-      </section>
-      <section className="team-links-2">
-        <div className="team-text-2">
+        <br/>
+        <div className="team-text">
           <div className="team-icon">
-            <BsFillFileEarmarkPersonFill size={180}/>
+            <BsFillFileEarmarkPersonFill size={180} />
+            <br/>
+            <h4>Barber 1</h4>
+            <h5>Barber </h5>
           </div>
-          <h4>Employee 2</h4>
-          <h5>Position</h5>
-          <br/>
-          <p>
-            Experience...
-          </p>
         </div>
-        <br/> <br/>
         <div className="team-text-2">
-          <div className="team-icon">
+          <div className="team-icon-2">
             <BsFillFileEarmarkPersonFill size={180}/>
+            <h4>Barber 2</h4>
+            <h5>Barber</h5>
           </div>
-          <h4>Employee 3</h4>
-          <h5>Position</h5>
-          <br/>
-          <p>
-            Experience...
-          </p>
         </div>
-      </section>
+        <br/>
+        <div className="team-text-2">
+          <div className="team-icon-2"> 
+            <BsFillFileEarmarkPersonFill size={180}/>
+            <h4>Barber 3</h4>
+            <h5>Barber</h5>
+        </div>
+        </div>
+      </div>
     </div>
   );
 };
