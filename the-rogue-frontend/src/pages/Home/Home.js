8d1cc0e3ad7db4a23cf97a8df6744231e2cee674
lmx97 <meixian971@gmail.com>
2023-07-08 18:04:00 +0800

    updated Home Page

--- the-rogue-frontend/src/pages/Home/Home.js
+++ the-rogue-frontend/src/pages/Home/Home.js
@@ -3,18 +3,19 @@ import Navbar from "../../components/navbar/Navbar";
 import Contact from "../Contact/Contact";
 import Services from "../Services/Services";
 import Team from "../Team/Team";
-import News from "../News/News";
-
+// import News from "../News/News";
+import Images from "../Images/Images";
+import Cover from "../Cover/Cover";
 
 const Home = () => {
     return (
         <div className="home" id="home">
             <Navbar />
-            <News />
+            <Cover />
             <Services />
+            <Images />
             <Team />
             <Contact />
-
         </div>
     );
 };
