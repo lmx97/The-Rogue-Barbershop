326c358a57de1d48321efe5dce7f3bc1c4efa6c3
lmx97 <meixian971@gmail.com>
2023-07-09 21:23:45 +0800

    remove unused path

--- the-rogue-frontend/src/App.js
+++ the-rogue-frontend/src/App.js
@@ -1,23 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
-import Services from "./pages/Services/Services";
-import News from "./pages/News/News";
-import Team from "./pages/Team/Team";
-import SimpleMap from "./components/GoogleMap/SimpleMap";
-import Cover from "./pages/Cover/Cover";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/team" element={<Team/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/simple" element={<SimpleMap />} />
-          <Route path="/Cover" element={<Cover />} />
         </Routes>
     </BrowserRouter>
   );
